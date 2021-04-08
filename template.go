0e3e1e7126b28b10ca19f4b5977f6e9bc127e592
MarvinJWendt <git@marvinjwendt.com>
2021-04-08 12:30:49 +0200

    docs(code): move package description to doc.go

--- template.go
+++ template.go
@@ -1,10 +1,3 @@
-/*
-Package template is a template for AtomicGo projects.
-
-Here is the place for the description of the module.
-
-You can use **Markdown** here.
-*/
 package template
 
 // HelloWorld returns `Hello, World!`.
